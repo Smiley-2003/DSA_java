7ebf6bbf54f58d59ac9832d5f9a98a48bf3acff4
Naina Mogha <92297363+Smiley-2003@users.noreply.github.com>
2025-06-10 00:14:04 +0530

    Add files via upload

---
+++ Lec4/check_prime.java
@@ -0,0 +1,22 @@
+package Lec4;
+
+public class check_prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int n = 7;
+      int fact = 0;
+      for(int  i = 2; i < n; i++) {
+    	  if(n % i == 0) {
+    		  fact++;
+    	    }
+         }
+    	  if(fact>=1) {
+    	     System.out.println("Not prime");
+          }else {
+        	 System.out.println("Prime");
+          }
+
+	}
+}
+	
---
+++ Lec4/forloopdemo.java
@@ -0,0 +1,18 @@
+package Lec4;
+
+public class forloopdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//     int i = 1;
+//     while(i<=n) {
+    	 int n = 5;
+    	 
+    	 for(int i = 1;i <= n; i++) {
+    		 System.out.println(i);
+    	 }
+    
+     
+	}
+
+}
